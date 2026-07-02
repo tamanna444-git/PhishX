3e1cf42b8d9f04e31d60b50ede5b208c0a552223
Maan1833 <maansingh1806@gmail.com>
2026-07-02 20:45:21 +0530

    add user registration API with FastAPI and SQLite

---
+++ backend/app/routes/user.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter,Depends
+from sqlalchemy.orm import Session
+from backend.app.database.database import get_db
+from backend.app.models.user import User
+from backend.app.schemas.user import UserCreate
+router= APIRouter()
+@router.get("/")
+def test():
+    return{"messsage":"user route working"}
+@router.post("/register")
+def register_user(user:UserCreate,db: Session=Depends(get_db)):
+    new_user =User(
+        name=user.name,
+        email=user.email,
+        password=user.password
+
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return{"mesage":"user registered sucessfully","id":new_user.id}
